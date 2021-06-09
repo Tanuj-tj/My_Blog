9056a17ab465aa3d8442f4209356170eeb441fbc
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-06-10 00:42:15 +0530

    final commit

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a8xt$3j)kdev$^8c0si%(c=coi3y6=&-^p6eekc@ziefrdc-eb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['techiecomet.herokuapp.com' , '127.0.0.1:8000']
 
 
 # Application definition
